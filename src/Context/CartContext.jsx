4ee3ccd842185aaa79084e93e3fb56f5eb0a8ba5
Shreyaskr8 <skrravi08@gmail.com>
2025-06-15 16:53:10 +0530

    add Order summary, style btns

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -5,6 +5,15 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
     const [cartItems, setCartItems] = useState([]);
 
+    
+    function getTotalCost() {
+        let totalCost = 0;
+        for(let i = 0; i < cartItems.length; i++) {
+            totalCost += (cartItems[i].cost * cartItems[i].quantity);
+        }
+        return totalCost;
+    }
+
     function addToCart(item) {
         setCartItems(prevCartItems => {
             const itemIndex = prevCartItems.findIndex(cartItem => cartItem.id === item.id);
@@ -33,7 +42,7 @@ export const CartProvider = ({ children }) => {
     }
 
     return(
-        <CartContext.Provider value={{ cartItems, addToCart, removeItemFromCart, removeAllItemsFromCart }}>
+        <CartContext.Provider value={{ cartItems, addToCart, removeItemFromCart, removeAllItemsFromCart, getTotalCost }}>
             {children}
         </CartContext.Provider>
     )
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -4,18 +4,42 @@ import CartItemCard from "../Components/CartItemCard";
 import './CartPage.css'
 
 function CartPage() {
-    const { cartItems, removeAllItemsFromCart } = useContext(CartContext);
-    console.log(cartItems);
+    const { cartItems, removeAllItemsFromCart, getTotalCost } = useContext(CartContext);
+    // console.log(cartItems);
+
+    const itemsTotalCost = getTotalCost();
+    let deliveryCost = 2;
+    if(itemsTotalCost === 0 || itemsTotalCost > 50) {
+        deliveryCost = 0;
+    }
 
     return(
         <>
             <h1 className="page-title">Cart</h1>
             <button className="delete-all-items-btn" onClick={removeAllItemsFromCart}>Clear Cart</button>
-            <div className="cart-item-section">
-                {cartItems.map((cartItem, index) => {
-                    return <CartItemCard key={index} item={cartItem}/>
-                }
-            )}
+            <div className="cart-section">
+                <div className="cart-item-section">
+                    <div className="item-col-names">
+                        <h3 className="game-col">Game</h3>
+                        <h3 className="cost-col">Cost</h3>
+                        <h3 className="qty-col">Qty</h3>
+                    </div>
+                    {cartItems.map((cartItem, index) => {
+                        return <CartItemCard key={index} item={cartItem}/>
+                    }
+                )}
+                </div>
+                <div className="order-summary-section">
+                    <header>
+                        <h2>Order Summary</h2>
+                    </header>
+                    <div className="cost-details">
+                        <p>Items Total: ${itemsTotalCost}</p>
+                        <p>Delivery cost: ${deliveryCost}</p>
+                        <p>Order Total: ${itemsTotalCost + deliveryCost}</p>
+                        <button className="buy-items-btn btn-fill-bottom btn-fill-bottom--green">Proceed to buy</button>
+                    </div>
+                </div>
             </div>
         </>
     )
