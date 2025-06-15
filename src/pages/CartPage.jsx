58ff331c69b0f8ec49c566a8b78ee179f1d9b2a9
Shreyaskr8 <skrravi08@gmail.com>
2025-06-15 21:17:43 +0530

    add handling empty cart

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,7 +5,7 @@ import './CartPage.css'
 
 function CartPage() {
     const { cartItems, removeAllItemsFromCart, getTotalCost } = useContext(CartContext);
-    // console.log(cartItems);
+    console.log(cartItems);
 
     const itemsTotalCost = getTotalCost();
     let deliveryCost = 2;
@@ -16,31 +16,39 @@ function CartPage() {
     return(
         <>
             <h1 className="page-title">Cart</h1>
-            <button className="delete-all-items-btn" onClick={removeAllItemsFromCart}>Clear Cart</button>
             <div className="cart-section">
-                <div className="cart-item-section">
-                    <div className="item-col-names">
-                        <h3 className="game-col">Game</h3>
-                        <h3 className="cost-col">Cost</h3>
-                        <h3 className="qty-col">Qty</h3>
+                {cartItems.length === 0 && (
+                    <div className="cart-empty-message">
+                        <p>Your cart is empty. Go add some games!</p>
                     </div>
-                    {cartItems.map((cartItem, index) => {
-                        return <CartItemCard key={index} item={cartItem}/>
-                    }
                 )}
-                </div>
-                <div className="order-summary-section">
-                    <header>
-                        <h2>Order Summary</h2>
-                    </header>
-                    <div className="cost-details">
-                        <p>Items Total: ${itemsTotalCost}</p>
-                        <p>Delivery cost: ${deliveryCost}</p>
-                        <p>Order Total: ${itemsTotalCost + deliveryCost}</p>
-                        <button className="buy-items-btn btn-fill-bottom btn-fill-bottom--green">Proceed to buy</button>
+                {cartItems.length > 0 && (
+                    <>
+                        <div className="cart-item-section">
+                            <div className="item-col-names">
+                                <h3 className="game-col">Game</h3>
+                                <h3 className="cost-col">Cost</h3>
+                                <h3 className="qty-col">Qty</h3>
+                            </div>
+                            {cartItems.map((cartItem, index) => (
+                                <CartItemCard key={index} item={cartItem} />
+                            ))}
+                        </div>
+                        <div className="order-summary-section">
+                        <header>
+                            <h2>Order Summary</h2>
+                        </header>
+                        <div className="cost-details">
+                            <p>Items Total: ${itemsTotalCost}</p>
+                            <p>Delivery cost: ${deliveryCost}</p>
+                            <p>Order Total: ${itemsTotalCost + deliveryCost}</p>
+                            <button className="buy-items-btn btn-fill-bottom btn-fill-bottom--green">Proceed to buy</button>
+                        </div>
                     </div>
-                </div>
+                </>
+                )}
             </div>
+            {cartItems.length > 0 && <button className="delete-all-items-btn" onClick={removeAllItemsFromCart}>Clear Cart</button>}
         </>
     )
 }
