a2f4a9320525338eb2f0baae077224276a83a1b4
Shreyaskr8 <skrravi08@gmail.com>
2025-06-20 00:52:26 +0530

    add favicon and update site name

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
     return (
         <>
             <p>Welcome to the Play Stack!</p>
+            <footer>
+                <a href="https://www.flaticon.com/free-icons/game-controller" title="game controller icons" className="icon-attribute">Game controller icons created by shmai - Flaticon</a>   
+            </footer>
         </>
     )
 }
